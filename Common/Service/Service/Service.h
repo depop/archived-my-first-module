ae311d81adec3cd71d6467dadd12b231849f0929
Nebil Kriedi <nebil@depop.com>
2019-03-07 17:15:02 +0000

    Added Service

---
+++ Common/Service/Service/Service.h
@@ -0,0 +1,19 @@
+//
+//  Service.h
+//  Service
+//
+//  Created by Nebil Kriedi on 07/03/2019.
+//  Copyright © 2019 Depop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Service.
+FOUNDATION_EXPORT double ServiceVersionNumber;
+
+//! Project version string for Service.
+FOUNDATION_EXPORT const unsigned char ServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Service/PublicHeader.h>
+
+
