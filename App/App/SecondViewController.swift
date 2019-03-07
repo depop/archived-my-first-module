06cb30c34b495ddb714e6d2f0a7adf988d0f6c10
Nebil Kriedi <nebil@depop.com>
2019-03-07 17:07:05 +0000

    Initial commit

---
+++ App/App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  App
+//
+//  Created by Nebil Kriedi on 07/03/2019.
+//  Copyright © 2019 Depop. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
