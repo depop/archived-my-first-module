871c887a039e4ed1edd5ed54bce68fcf17c7b1c8
Nebil Kriedi <nebil@depop.com>
2019-03-07 17:20:36 +0000

    Added UserProfile

---
+++ Modules/UserProfile/UserProfile/UserProfile.h
@@ -0,0 +1,19 @@
+//
+//  UserProfile.h
+//  UserProfile
+//
+//  Created by Nebil Kriedi on 07/03/2019.
+//  Copyright © 2019 Depop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserProfile.
+FOUNDATION_EXPORT double UserProfileVersionNumber;
+
+//! Project version string for UserProfile.
+FOUNDATION_EXPORT const unsigned char UserProfileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserProfile/PublicHeader.h>
+
+
