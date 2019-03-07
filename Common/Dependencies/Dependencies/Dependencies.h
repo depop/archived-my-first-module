ea38971b50cffca57ff022a1ab323d4905c63883
Nebil Kriedi <nebil@depop.com>
2019-03-07 17:22:53 +0000

    Added Dependencies

---
+++ Common/Dependencies/Dependencies/Dependencies.h
@@ -0,0 +1,19 @@
+//
+//  Dependencies.h
+//  Dependencies
+//
+//  Created by Nebil Kriedi on 07/03/2019.
+//  Copyright © 2019 Depop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dependencies.
+FOUNDATION_EXPORT double DependenciesVersionNumber;
+
+//! Project version string for Dependencies.
+FOUNDATION_EXPORT const unsigned char DependenciesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dependencies/PublicHeader.h>
+
+
