88e402b8ce8d875fee0d1497701c7d5e24e9ee09
Nebil Kriedi <nebil@depop.com>
2019-03-07 17:10:02 +0000

    Added Utils

---
+++ Common/Utils/Utils/Utils.h
@@ -0,0 +1,19 @@
+//
+//  Utils.h
+//  Utils
+//
+//  Created by Nebil Kriedi on 07/03/2019.
+//  Copyright © 2019 Depop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Utils.
+FOUNDATION_EXPORT double UtilsVersionNumber;
+
+//! Project version string for Utils.
+FOUNDATION_EXPORT const unsigned char UtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utils/PublicHeader.h>
+
+
