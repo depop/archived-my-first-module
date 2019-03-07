e3a721a2dba1bd0f17f71092bb47eaebf6a03f75
Nebil Kriedi <nebil@depop.com>
2019-03-07 17:19:10 +0000

    Added Chat

---
+++ Modules/Chat/Chat/Chat.h
@@ -0,0 +1,19 @@
+//
+//  Chat.h
+//  Chat
+//
+//  Created by Nebil Kriedi on 07/03/2019.
+//  Copyright © 2019 Depop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chat.
+FOUNDATION_EXPORT double ChatVersionNumber;
+
+//! Project version string for Chat.
+FOUNDATION_EXPORT const unsigned char ChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chat/PublicHeader.h>
+
+
