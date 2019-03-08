8ff9214b7323f37006cce0cb074cca9c2ade800b
Nebil Kriedi <nebil@depop.com>
2019-03-08 18:06:58 +0000

    Content for user profile controller

--- Modules/UserProfile/UserProfile/UserProfileViewController.swift
+++ Modules/UserProfile/UserProfile/UserProfileViewController.swift
@@ -28,6 +28,24 @@ class UserProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor.blue
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Chat", style: .plain, target: self, action: #selector(openChat))
+        showLabel()
+    }
+    
+    private func showLabel() {
+        // add a label with user data
+        let label = UILabel(frame: CGRect.zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textColor = UIColor.yellow
+        label.text = "User: \(user.identifier)"
+        label.font = UIFont.systemFont(ofSize: 30)
+        let constraints = [
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 10)
+        ]
+        view.addSubview(label)
+        NSLayoutConstraint.activate(constraints)
     }
     
     @objc func openChat() {
